342d6e0123b704bd2a187a16db6807a0b2430d0d
pranjal-tank <pranjalt7869@gmail.com>
2022-08-25 12:11:01 +0530

    update some files

--- Ppolls/settings.py
+++ Ppolls/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +130,3 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Activate Django heroku
-django_heroku.settings(locals())
